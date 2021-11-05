d2c02f0b80e6644f26aeea58bc94bbaf8ecfafeb
hslyu <hslyu4@gmail.com>
2021-11-06 00:36:09 +0900

    ADD Qplayer

--- connect4.py
+++ connect4.py
@@ -9,8 +9,13 @@
 
 import random
 import os
-import time
-from minimax import Minimax
+from player import *
+import cfg
+import numpy as np
+import logging
+
+WIDTH = cfg.WIDTH
+HEIGHT = cfg.HEIGHT
 
 class Game(object):
     """ Game object that holds state of Connect 4 board and game values
@@ -25,49 +30,39 @@ class Game(object):
     game_name = u"Connecter Quatre\u2122" # U+2122 is "tm" this is a joke
     colors = ["x", "o"]
     
-    def __init__(self):
+    def __init__(self, width = WIDTH, height = HEIGHT, verbose=True):
         self.round = 1
         self.finished = False
         self.winner = None
+        self.width = width
+        self.height = height
+        self.verbose = verbose
         
+        # logging
+        logging.basicConfig(format='%(message)s', level=logging.DEBUG if verbose else logging.INFO) 
+        self.logger = logging.getLogger("connect4")
+        self.handler = logging.StreamHandler()
+        self.handler.setLevel(logging.DEBUG if verbose else logging.INFO) 
+        
+        self.formatter = logging.Formatter('%(message)s')
+        self.handler.setFormatter(self.formatter)
+        
+        diff = 4
         # do cross-platform clear screen
         os.system( [ 'clear', 'cls' ][ os.name == 'nt' ] )
-        print(u"Welcome to {0}!".format(self.game_name))
-        print("Should Player 1 be a Human or a Computer?")
-        while self.players[0] == None:
-            choice = str(input("Type 'H' or 'C': "))
-            if choice == "Human" or choice.lower() == "h":
-                name = str(input("What is Player 1's name? "))
-                self.players[0] = Player(name, self.colors[0])
-            elif choice == "Computer" or choice.lower() == "c":
-                name = str(input("What is Player 1's name? "))
-                diff = int(input("Enter difficulty for this AI (1 - 4) "))
-                self.players[0] = AIPlayer(name, self.colors[0], diff+1)
-            else:
-                print("Invalid choice, please try again")
-        print("{0} will be {1}".format(self.players[0].name, self.colors[0]))
+        self.players[0] = MiniMaxPlayer("Player 1", self.colors[0], 4)
+        self.logger.debug("{0} will be {1}".format(self.players[0].name, self.colors[0]))
         
-        print("Should Player 2 be a Human or a Computer?")
-        while self.players[1] == None:
-            choice = str(input("Type 'H' or 'C': "))
-            if choice == "Human" or choice.lower() == "h":
-                name = str(input("What is Player 2's name? "))
-                self.players[1] = Player(name, self.colors[1])
-            elif choice == "Computer" or choice.lower() == "c":
-                name = str(input("What is Player 2's name? "))
-                diff = int(input("Enter difficulty for this AI (1 - 4) "))
-                self.players[1] = AIPlayer(name, self.colors[1], diff+1)
-            else:
-                print("Invalid choice, please try again")
-        print("{0} will be {1}".format(self.players[1].name, self.colors[1]))
+        self.players[1] = MiniMaxPlayer("Player 2", self.colors[1], diff+1)
+        self.logger.debug("{0} will be {1}".format(self.players[1].name, self.colors[1]))
         
     # x always goes first (arbitrary choice on my part)
         self.turn = self.players[0]
         
         self.board = []
-        for i in range(6):
+        for i in range(self.height):
             self.board.append([])
-            for j in range(7):
+            for j in range(self.width):
                 self.board[i].append(' ')
     
     def newGame(self):
@@ -81,9 +76,9 @@ class Game(object):
         self.turn = self.players[0]
         
         self.board = []
-        for i in range(6):
+        for i in range(self.height):
             self.board.append([])
-            for j in range(7):
+            for j in range(self.width):
                 self.board[i].append(' ')
 
     def switchTurn(self):
@@ -100,30 +95,31 @@ class Game(object):
 
         # there are only 42 legal places for pieces on the board
         # exactly one piece is added to the board each turn
-        if self.round > 42:
+        if self.round > self.width*self.height:
             self.finished = True
             # this would be a stalemate :(
             return
         
         # move is the column that player want's to play
+        self.logger.debug("{0}'s turn.  {0} is {1}".format(player.name, player.color))
         move = player.move(self.board)
 
-        for i in range(6):
+        for i in range(self.height):
             if self.board[i][move] == ' ':
                 self.board[i][move] = player.color
                 self.switchTurn()
                 self.checkForFours()
-                self.printState()
+                if self.verbose:
+                    self.printState()
                 return
-
         # if we get here, then the column is full
-        print("Invalid move (column is full)")
+        self.logger.debug("Invalid move (column is full)")
         return
     
     def checkForFours(self):
         # for each piece in the board...
-        for i in range(6):
-            for j in range(7):
+        for i in range(self.height):
+            for j in range(self.width):
                 if self.board[i][j] != ' ':
                     # check if a vertical four-in-a-row starts at (i, j)
                     if self.verticalCheck(i, j):
@@ -139,7 +135,7 @@ class Game(object):
                     # also, get the slope of the four if there is one
                     diag_fours, slope = self.diagonalCheck(i, j)
                     if diag_fours:
-                        print(slope)
+                        self.logger.debug(slope)
                         self.finished = True
                         return
         
@@ -148,7 +144,7 @@ class Game(object):
         fourInARow = False
         consecutiveCount = 0
     
-        for i in range(row, 6):
+        for i in range(row, self.height):
             if self.board[i][col].lower() == self.board[row][col].lower():
                 consecutiveCount += 1
             else:
@@ -167,7 +163,7 @@ class Game(object):
         fourInARow = False
         consecutiveCount = 0
         
-        for j in range(col, 7):
+        for j in range(col, self.width):
             if self.board[row][j].lower() == self.board[row][col].lower():
                 consecutiveCount += 1
             else:
@@ -190,8 +186,8 @@ class Game(object):
         # check for diagonals with positive slope
         consecutiveCount = 0
         j = col
-        for i in range(row, 6):
-            if j > 6:
+        for i in range(row, self.height):
+            if j >= self.height:
                 break
             elif self.board[i][j].lower() == self.board[row][col].lower():
                 consecutiveCount += 1
@@ -211,7 +207,7 @@ class Game(object):
         consecutiveCount = 0
         j = col
         for i in range(row, -1, -1):
-            if j > 6:
+            if j >= self.height:
                 break
             elif self.board[i][j].lower() == self.board[row][col].lower():
                 consecutiveCount += 1
@@ -238,8 +234,8 @@ class Game(object):
             Calls highlightFours
         """
     
-        for i in range(6):
-            for j in range(7):
+        for i in range(self.height):
+            for j in range(self.width):
                 if self.board[i][j] != ' ':
                     # check if a vertical four-in-a-row starts at (i, j)
                     if self.verticalCheck(i, j):
@@ -278,80 +274,29 @@ class Game(object):
                     self.board[row-i][col+i] = self.board[row-i][col+i].upper()
         
         else:
-            print("Error - Cannot enunciate four-of-a-kind")
+            self.logger.debug("Error - Cannot enunciate four-of-a-kind")
     
     def printState(self):
         # cross-platform clear screen
         os.system( [ 'clear', 'cls' ][ os.name == 'nt' ] )
-        print(u"{0}!".format(self.game_name))
-        print("Round: " + str(self.round))
+        self.logger.debug(u"{0}!".format(self.game_name))
+        self.logger.debug("Round: " + str(self.round))
 
-        for i in range(5, -1, -1):
+        for i in range(self.height-1, -1, -1):
             print("\t", end="")
-            for j in range(7):
+            for j in range(self.width):
                 print("| " + str(self.board[i][j]), end=" ")
             print("|")
-        print("\t  _   _   _   _   _   _   _ ")
-        print("\t  1   2   3   4   5   6   7 ")
+        self.logger.debug("\t "+" _  "*self.width)
+        column_string = ''
+        for i in range(1,self.width+1):
+            column_string += f' {i}  '
+        self.logger.debug("\t " + column_string)
 
         if self.finished:
-            print("Game Over!")
+            self.logger.debug("Game Over!")
             if self.winner != None:
-                print(str(self.winner.name) + " is the winner")
+                self.logger.debug(str(self.winner.name) + " is the winner")
             else:
-                print("Game was a draw")
+                self.logger.debug("Game was a draw")
                 
-class Player(object):
-    """ Player object.  This class is for human players.
-    """
-    
-    type = None # possible types are "Human" and "AI"
-    name = None
-    color = None
-    def __init__(self, name, color):
-        self.type = "Human"
-        self.name = name
-        self.color = color
-    
-    def move(self, state):
-        print("{0}'s turn.  {0} is {1}".format(self.name, self.color))
-        column = None
-        while column == None:
-            try:
-                choice = int(input("Enter a move (by column number): ")) - 1
-            except ValueError:
-                choice = None
-            if 0 <= choice <= 6:
-                column = choice
-            else:
-                print("Invalid choice, try again")
-        return column
-
-
-class AIPlayer(Player):
-    """ AIPlayer object that extends Player
-        The AI algorithm is minimax, the difficulty parameter is the depth to which 
-        the search tree is expanded.
-    """
-    
-    difficulty = None
-    def __init__(self, name, color, difficulty=5):
-        self.type = "AI"
-        self.name = name
-        self.color = color
-        self.difficulty = difficulty
-        
-    def move(self, state):
-        print("{0}'s turn.  {0} is {1}".format(self.name, self.color))
-        
-        # sleeping for about 1 second makes it looks like he's thinking
-        #time.sleep(random.randrange(8, 17, 1)/10.0)
-        #return random.randint(0, 6)
-        
-        m = Minimax(state)
-        best_move, value = m.bestMove(self.difficulty, state, self.color)
-        return best_move
-
-
-
-
